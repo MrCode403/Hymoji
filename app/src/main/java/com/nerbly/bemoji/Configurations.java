7acfb4fae7c903bbe06526d4eb8ac9f63d2ac3db
Ilyasse <ilyassesalama7@gmail.com>
2022-05-26 21:33:07 +0100

    feat: Changed emojis images source

--- app/src/main/java/com/nerbly/bemoji/Configurations.java
+++ app/src/main/java/com/nerbly/bemoji/Configurations.java
@@ -2,7 +2,7 @@ package com.nerbly.bemoji;
 
 public class Configurations {
 
-    public static String ASSETS_SOURCE_LINK = "https://emoji.gg/assets/emoji/";
+    public static String ASSETS_SOURCE_LINK = "https://cdn3.emoji.gg/emojis/";
     public static String EMOJIS_API_LINK = "https://emoji.gg/api/";
     public static String PACKS_API_LINK = "https://emoji.gg/api/packs";
     public static String CATEGORIES_API_LINK = "https://emoji.gg/api/?request=categories";
